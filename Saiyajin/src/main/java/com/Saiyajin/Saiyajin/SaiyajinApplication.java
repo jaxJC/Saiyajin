d3609c69c9f5f74ec4a9e2127d73de20ec130a1b
jaxJC <seg.valdebenito@duocuc.cl>
2025-04-23 16:32:51 -0400

    hola mundo

---
+++ Saiyajin/src/main/java/com/Saiyajin/Saiyajin/SaiyajinApplication.java
@@ -0,0 +1,13 @@
+package com.Saiyajin.Saiyajin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaiyajinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaiyajinApplication.class, args);
+	}
+
+}
---
+++ Saiyajin/src/test/java/com/Saiyajin/Saiyajin/SaiyajinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Saiyajin.Saiyajin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaiyajinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
